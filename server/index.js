4e505d0627b3ecd50fffde09580f1f167e6f29de
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-09-17 17:56:28 +0530

    MongoDB connection

--- server/index.js
+++ server/index.js
@@ -9,8 +9,20 @@ dotenv.config(); // Load Environment variables from .env to process.env
 
 const app = express();
 
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URL);
+    if (conn) {
+      console.log(`📶 Connected to MongoDB \n`);
+    }
+  } catch (err) {
+    console.log(`❌ Error connecting MongoDB \n`, err);
+  }
+};
+
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`📞 Server is listening on PORT number ${PORT}. \n`);
+  connectDB();
 });
