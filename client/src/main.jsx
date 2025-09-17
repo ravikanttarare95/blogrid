abc0fc95f9960eb57369d1b1b0cdeeb2a648e865
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-09-17 18:12:08 +0530

    Client set up

---
+++ client/src/main.jsx
@@ -0,0 +1,6 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Home from "./Home.jsx";
+
+const root = createRoot(document.getElementById("root"));
+root.render(<Home />);
