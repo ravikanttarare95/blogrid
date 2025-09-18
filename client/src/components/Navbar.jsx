b3bad9eea68efa3c31cff9d0c8d09b05a85424f5
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-09-18 21:24:35 +0530

    Set up Routing

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,32 @@
 import React from "react";
+import { Link } from "react-router";
 import Logo from "./../../public/logo.png";
 
 function Navbar() {
   return (
-    <nav className="bg-gray-900 text-white shadow-md px-6 py-4">
+    <nav className="bg-gray-900 text-white shadow-md px-6 py-4 flex flex-col md:flex-row md:justify-between">
       <div className="flex items-center gap-3">
-        <img src={Logo} alt="Logo" className="w-18 object-contain" />
-        <span className="text-2xl font-bold text-white">BloGrid</span>
+        <img src={Logo} alt="Logo" className="w-16 rounded-lg" />
+        <span className="text-2xl font-extrabold bg-gradient-to-r from-teal-300 to-green-300 bg-clip-text text-transparent">
+          BloGrid
+        </span>
+      </div>
+      <div className="mt-2 flex gap-10 text-lg">
+        <Link to="/" className="hover:underline">
+          Home
+        </Link>
+        <Link to="/sign-up" className="hover:underline">
+          Sign Up
+        </Link>
+        <Link to="/login" className="hover:underline">
+          Login
+        </Link>
+        <Link to="/all-blogs" className="hover:underline">
+          All Blogs
+        </Link>
+        <Link to="/new-blog" className="hover:underline">
+          New Blog
+        </Link>
       </div>
     </nav>
   );
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,25 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import Home from "./views/Home";
+import SignUp from "./views/SignUp";
+import Login from "./views/Login";
+import AllBlogs from "./views/AllBlogs";
+import EditBlog from "./views/EditBlog";
+import NewBlog from "./views/NewBlog";
+import ReadBlog from "./views/ReadBlog";
+
+import { BrowserRouter, Routes, Route } from "react-router";
 const root = createRoot(document.getElementById("root"));
-root.render(<Home />);
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/all-blogs" element={<AllBlogs />} />
+      <Route path="/edit-blog" element={<EditBlog />} />
+      <Route path="/new-blog" element={<NewBlog />} />
+      <Route path="/read-blog" element={<ReadBlog />} />
+    </Routes>
+  </BrowserRouter>
+);
