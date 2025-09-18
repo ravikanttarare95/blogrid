dbd9b3a3af53a6199ee9ee886d93a9cd6fb49dd2
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-09-18 21:03:58 +0530

    installed libs and pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,5 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Home from "./Home.jsx";
-
+import Home from "./views/Home";
 const root = createRoot(document.getElementById("root"));
 root.render(<Home />);
