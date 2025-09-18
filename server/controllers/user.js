b3784575c4c5a24b38a26ad7aaae8fb84f0d6d54
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-09-18 18:46:16 +0530

    validations for Sign Up

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -3,6 +3,53 @@ import User from "./../models/User.js"; // import modelSchema
 const postSignup = async (req, res) => {
   const { name, email, password } = req.body;
 
+  if (!name) {
+    return res.json({ success: false, message: "Name is required" });
+  }
+  if (!email) {
+    return res.json({ success: false, message: "Email is required" });
+  }
+  if (!password) {
+    return res.json({ success: false, message: "Password is required" });
+  }
+
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+    res.json({
+      success: false,
+      message: `User with email ${email} already exists`,
+    });
+  }
+
+  const nameRegexTest = /^[A-Za-z\s]{2,30}$/;
+  const emailRegexTest = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const passwordRegexTest =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+  if (nameRegexTest.test(name) === false) {
+    return res.json({
+      success: false,
+      message:
+        "Name must be 2-30 characters long and contain only letters and spaces",
+    });
+  }
+
+  if (emailRegexTest.test(email) === false) {
+    return res.json({
+      success: false,
+      message: "Please enter a valid email address.",
+    });
+  }
+
+  if (passwordRegexTest.test(password) === false) {
+    return res.json({
+      success: false,
+      message:
+        "Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character",
+    });
+  }
+
   const userData = new User({ name, email, password });
 
   const savedData = await userData.save();
