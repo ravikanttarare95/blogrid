a4c4ef78928bb4fe69406c7ed9c373adc712bc84
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-09-18 14:58:14 +0530

    Comments model

--- server/models/User.js
+++ server/models/User.js
@@ -1,10 +1,13 @@
 import { model, Schema, schema } from "mongoose";
 
-const userSchema = new Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
 const User = ("User", userSchema);
 
