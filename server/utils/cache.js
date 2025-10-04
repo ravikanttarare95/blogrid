9e484f36d0f244aa1d649ef271566e33a9e5f0ad
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-10-04 14:00:42 +0530

    added cache code

---
+++ server/utils/cache.js
@@ -0,0 +1,31 @@
+import { createClient } from "redis";
+import dotenv from "dotenv";
+dotenv.config();
+
+const client = createClient({
+  password: process.env.REDIS_PASSWORD,
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+  },
+});
+
+client.on("error", (err) => console.log("Redis Client Error", err));
+
+await client.connect();
+
+const setCache = async (key, value) => {
+  await client.set(key, JSON.stringify(value));
+  return true;
+};
+
+const getCache = async (key) => {
+  const value = await client.get(key);
+  return JSON.parse(value);
+};
+
+const clearCache = async () => {
+  await client.sendCommand(["FLUSHALL"]);
+};
+
+export { setCache, getCache, clearCache };
