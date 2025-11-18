47c0c3d4bdd79c15a52a9adc503b1f7350885cc6
ravikanttarare95 <ravikanttarare2001@gmail.com>
2025-11-19 01:25:33 +0530

    Style MyBlog

--- client/src/views/MyBlogs.jsx
+++ client/src/views/MyBlogs.jsx
@@ -10,9 +10,10 @@ import BlogCardSkeleton from "./../components/BlogCardSkeleton";
 import BlogCard from "./../components/BlogCard";
 
 function MyBlogs() {
-  const [user, setUser] = useState(getCurrentUser());
+  const [user] = useState(getCurrentUser());
   const [draftBlogs, setDraftBlogs] = useState(null);
   const [publishedBlogs, setPublishedBlogs] = useState(null);
+  const [showDraft, setShowDraft] = useState(true);
   const navigate = useNavigate();
 
   const loadMyDraftBlogs = async () => {
@@ -25,7 +26,7 @@ function MyBlogs() {
       );
 
       if (response?.data?.success) {
-        setDraftBlogs(response?.data?.data);
+        setDraftBlogs(response?.data?.data || []);
       }
     } catch (error) {
       console.log(error);
@@ -44,7 +45,7 @@ function MyBlogs() {
       );
 
       if (response?.data?.success) {
-        setPublishedBlogs(response?.data?.data);
+        setPublishedBlogs(response?.data?.data || []);
       }
     } catch (error) {
       console.log(error);
@@ -55,64 +56,75 @@ function MyBlogs() {
     if (!user) return navigate("/login");
     loadMyDraftBlogs();
     loadMyPublishedBlogs();
-  }, [user]);
+  }, []);
+
+  const blogsToShow = showDraft ? draftBlogs : publishedBlogs;
 
   return (
     <>
       <Navbar isSelected={"/my-blogs"} />
-      {draftBlogs === null ? (
-        <div className="p-5 pb-0 sm:pb-0 sm:p-8 flex flex-col gap-10 my-5">
-          {Array.from({ length: 5 }).map((_, index) => (
-            <BlogCardSkeleton key={index} />
-          ))}
-        </div>
-      ) : draftBlogs.length > 0 ? (
-        <div className="min-h-[68vh] p-5 pb-0 sm:pb-0 sm:p-8 flex flex-col gap-10 my-5">
-          {draftBlogs.map((draftBlog) => (
+      <div className="flex gap-4 px-6 mt-4">
+        <Button
+          btnTitle="Drafts"
+          btnVariant={showDraft ? "primary" : "secondary"}
+          onBtnClick={() => setShowDraft(true)}
+        />
+        <Button
+          btnTitle="Published"
+          btnVariant={!showDraft ? "primary" : "secondary"}
+          onBtnClick={() => setShowDraft(false)}
+        />
+      </div>
+      <div className="min-h-[59vh] p-5 pb-0 sm:pb-0 sm:p-8 flex flex-col gap-10 my-5">
+        {draftBlogs === null || publishedBlogs === null ? (
+          <>
+            {Array.from({ length: 5 }).map((_, index) => (
+              <BlogCardSkeleton key={index} />
+            ))}
+          </>
+        ) : blogsToShow.length > 0 ? (
+          blogsToShow.map((blog) => (
             <BlogCard
-              key={draftBlog._id}
-              blogId={draftBlog._id}
-              title={draftBlog.title}
-              content={draftBlog.content}
-              status={draftBlog.status}
-              category={draftBlog.category}
-              publishedAt={draftBlog.publishedAt}
-              createdAt={draftBlog.createdAt}
-              author={draftBlog.author}
-              slug={draftBlog.slug}
-              viewCount={draftBlog.viewCount}
-              likes={draftBlog.likes}
-              imgURL={draftBlog.imgURL}
+              key={blog._id}
+              blogId={blog._id}
+              title={blog.title}
+              content={blog.content}
+              status={blog.status}
+              category={blog.category}
+              publishedAt={blog.publishedAt}
+              createdAt={blog.createdAt}
+              author={blog.author}
+              slug={blog.slug}
+              viewCount={blog.viewCount}
+              likes={blog.likes}
+              imgURL={blog.imgURL}
               //   isFavourite={draftBlogs.some((fav) => fav._id === draftBlog._id)}
               // onFavouriteToggle={onFavouriteToggle}
             />
-          ))}
-        </div>
-      ) : (
-        // <BlogCard />
+          ))
+        ) : (
+          <div className=" flex flex-col items-center justify-center text-center px-4">
+            <div className="p-6 rounded-full bg-teal-50 border border-teal-200 mb-6 shadow-md">
+              <NotebookPen className="w-14 h-14 text-teal-600 animate-bounce" />
+            </div>
 
-        <div className="min-h-[70vh] flex flex-col items-center justify-center text-center px-4">
-          <div className="p-6 rounded-full bg-teal-50 border border-teal-200 mb-6 shadow-md">
-            <NotebookPen className="w-14 h-14 text-teal-600 animate-bounce" />
+            <h2 className="text-2xl sm:text-3xl font-semibold text-gray-800 mb-3">
+              No {showDraft ? "draft" : "published"} blogs available.
+            </h2>
+
+            <p className="text-gray-600 max-w-md mb-6">
+              You haven’t created any blogs yet. Start your first blog now!
+            </p>
+
+            <Button
+              btnTitle={"Create Blog"}
+              btnSize="md"
+              btnVariant="primary"
+              onBtnClick={() => navigate("/new")}
+            />
           </div>
-
-          <h2 className="text-2xl sm:text-3xl font-semibold text-gray-800 mb-3">
-            No Blogs Yet
-          </h2>
-
-          <p className="text-gray-600 max-w-md mb-6">
-            You haven’t created any blogs yet. Start your first blog now!
-          </p>
-
-          <Button
-            btnTitle={"Create Blog"}
-            btnSize="md"
-            btnVariant="primary"
-            onBtnClick={() => navigate("/new")}
-          />
-        </div>
-      )}
-
+        )}
+      </div>
       <Footer />
     </>
   );
